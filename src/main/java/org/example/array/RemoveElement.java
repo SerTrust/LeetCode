4a39713d61bd55b49e01f908c0d67ba5d7c3509a
MSI\User <sunkistfool@gmail.com>
2021-06-30 18:19:43 +0800

    1.update RemoveElement

---
+++ src/main/java/org/example/array/RemoveElement.java
@@ -0,0 +1,20 @@
+package org.example.array;
+
+public class RemoveElement {
+
+    public static void main(String[] args) {
+        int[] nums = {3,2,2,3};
+        System.out.println(removeElement(nums, 2));
+    }
+
+    public static int removeElement(int[] nums, int val) {
+        if (nums.length == 0) return 0;
+        int position = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != val){
+                nums[position++] = nums[i];
+            }
+        }
+        return position;
+    }
+}
