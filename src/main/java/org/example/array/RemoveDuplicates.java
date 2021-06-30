eaae04b505933278270e76046e68fba1c12910f5
MSI\User <sunkistfool@gmail.com>
2021-06-30 10:38:56 +0800

    1.update RemoveDuplicates

---
+++ src/main/java/org/example/array/RemoveDuplicates.java
@@ -0,0 +1,20 @@
+package org.example.array;
+
+public class RemoveDuplicates {
+
+    public static void main(String[] args) {
+        int[] nums = {1,1,2};
+        System.out.println(removeDuplicates(nums));
+    }
+
+    public static int removeDuplicates(int[] nums) {
+        if (nums.length <= 1) return nums.length;
+        int position = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[position] != nums[i]){
+                nums[++position] = nums[i];
+            }
+        }
+        return position+1;
+    }
+}
