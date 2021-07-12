2e6f3b9146683b9905123247a981bd1b1d882014
chihyao <sunkistfool@gmail.com>
2021-07-12 20:11:33 +0800

    update 4sumII

--- src/main/java/org/example/HashTable/FourSumCount.java
+++ src/main/java/org/example/HashTable/FourSumCount.java
@@ -33,4 +33,21 @@ public class FourSumCount {
 
         return count;
     }
+
+    public static int fourSumCount2(int[] nums1, int[] nums2, int[] nums3, int[] nums4) {
+        Map<Integer, Integer> map=new HashMap<>();
+        for(int i=0;i<nums1.length;i++){
+            for(int j=0;j<nums2.length;j++){
+                int count = map.getOrDefault(nums1[i] + nums2[j], 0);
+                map.put(nums1[i] + nums2[j], count+1);
+            }
+        }
+        int res=0;
+        for(int i=0;i<nums3.length;i++){
+            for(int j=0;j<nums4.length;j++){
+                res+= map.getOrDefault(-nums3[i] -nums4[j], 0);
+            }
+        }
+        return res;
+    }
 }
